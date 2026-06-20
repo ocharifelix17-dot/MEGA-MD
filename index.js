ac3e601c633d17322f11d19105ab354b500884a1
ocharifelix17-dot <ocharifelix17@gmail.com>
2026-06-20 18:14:09 +0300

    Update default phone number in index.js

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ setInterval(() => {
         process.exit(1);
     }
 }, 30000);
-const phoneNumber = config.pairingNumber || config.ownerNumber || "923051391005";
+const phoneNumber = config.pairingNumber || config.ownerNumber || "+254750155834";
 // Auto-create data directory and default files on startup
 const DATA_DEFAULTS = {
     'owner.json': [],
